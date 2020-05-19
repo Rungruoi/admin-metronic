99db2d72a0a972cda1049e9d86925f190a98e124
Rungruoi <rungruoi99it@gmail.com>
2020-05-19 22:18:12 +0700

    package admin metronic v1

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace scuti\theme\admin\metronic;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+
+        $this->loadViewsFrom(__DIR__.'/views', 'admin');
+
+        $this->publishes([
+            __DIR__.'/views' => base_path('resources/views/'),
+            __DIR__.'/public' => base_path('public/'),
+        ], "admin-metronic");
+    }
+}
